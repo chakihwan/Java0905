8069c99376115a1b0e8e488fb0a9158e821cf376
chakihwan <rlghks0720@gmail.com>
2024-10-30 11:43:57 +0900

    JDBC Programming & 패키지를 MVC 모델링에 따라 분류하여 사용2

---
+++ src/jdbc3/controller/MainController.java
@@ -0,0 +1,20 @@
+package jdbc3.controller;
+
+import jdbc.JDBCConnector;
+import jdbc3.entity.Product;
+import jdbc3.view.ProductView;
+import jdbc3.repository.ProductRepository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class MainController {
+    public static void main(String[] args) {
+        ArrayList<Product> arrayList = ProductRepository.getList();
+        ProductView.printView(arrayList);
+    }
+}
